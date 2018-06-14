79d1177cfedf7843ac84946327adbe97b5a6cbc0
nastefanov <37981705+nastefanov@users.noreply.github.com>
2018-06-14 11:30:42 +0000

    Update Problem_1.R

--- Problem_1.R
+++ Problem_1.R
@@ -1,27 +1,52 @@
-## Homework 4, Problem 1
+# entering the data
 
-library(dplyr)
+housework <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
 
-## Read the data
-houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
-str(houseWork)
+# a)
 
-## a)
+summary(housework)
 
-## b)
+# b)
 
-## c)
+str(housework)
+muf <- mean(subset(housework$hours, housework$sex == 'f'))
+mum <- mean(subset(housework$hours, housework$sex == 'm'))
 
-## d)
+# c)
 
-## e)
+housework$female <- ifelse(housework$sex == "f", 'TRUE', 'FALSE')
+housework$male <- ifelse(housework$sex == "m", 'TRUE', 'FALSE')
 
-## f)
+# d)
 
-## g)
+model1 <- lm(hours ~ female, data = housework)
 
-## h)
+# e)
 
-## i)
+summary(model1)
 
-## j)
+# with this model we measure the linear connection between the hours spend in
+# houseworking and the female representatives who do it
+# the coefficients show that the houseworking hours per week will get down with
+# aproximateley 14.46 units if more females accomplish housework 
+# (both measured in coditional mean)
+# if only the number of males who do housework remain constant
+# looking at the P-value we can reject the null hypothesis
+# so we can coclude that there is conection between the time female spend
+# in houseworking and the mean  hours per week for keeping up the household clean
+# the R square is very low
+
+# f)
+
+# g)
+
+# h)
+
+# i)
+
+#j)
+
+model2 <- lm(hours ~ female + male, data = housework)
+summary(model2)
+
+# there are no male recordings in the data we use
